a33b89f4c31b5bd28cd6a775c5dc9c9505b09461
mdmcginnis <mdlavey@alumni.iastate.edu>
2020-03-25 10:39:26 -0500

    remove current active when setting new active

--- js/activeHeader.js
+++ js/activeHeader.js
@@ -3,5 +3,9 @@ $(document).ready(function() {
     var page = root.children[0].id
 
     var headerLink = document.querySelector("a#" + page)
+
+    var currentActive = document.querySelector("a.active")
+    currentActive.classList.toggle("active");
+
     headerLink.classList.toggle("active");
 });
