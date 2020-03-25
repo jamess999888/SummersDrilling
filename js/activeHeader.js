a06595bad1dd91a703ce5b5260d3f15d4162a97c
mdmcginnis <mdlavey@alumni.iastate.edu>
2020-03-25 10:49:43 -0500

    fixing null active issue

--- js/activeHeader.js
+++ js/activeHeader.js
@@ -5,7 +5,9 @@ $(document).ready(function() {
     var headerLink = document.querySelector("a#" + page)
 
     var currentActive = document.querySelector("a.active")
-    currentActive.classList.toggle("active");
+    if (currentActive) {
+        currentActive.classList.toggle("active");
+    }
 
     headerLink.classList.toggle("active");
 });
