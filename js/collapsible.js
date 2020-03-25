bac36d4ba7ae466c025a5b4582a6119267f82f5a
mdmcginnis <mdlavey@alumni.iastate.edu>
2020-03-25 10:52:31 -0500

    fixing collapsible

--- js/collapsible.js
+++ js/collapsible.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     for (i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function() {
             this.classList.toggle("active");
-            var content = this.parent.nextElementSibling;
+            var content = this.parentElement.nextElementSibling;
             if (content.style.display === "block") {
                 content.style.display = "none";
             } else {
