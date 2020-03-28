3613f9c6e0350011646d2f257f5a5ee3dafdf2ab
James McGinnis <mcginnis.james.j@gmail.com>
2020-03-27 20:01:50 -0500

    javascript to jquery in collapsable code

--- js/collapsible.js
+++ js/collapsible.js
@@ -1,19 +1,7 @@
 $(document).ready(function() {
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
-
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            var innerDiv = content.firstElementChild;
-            if (content.style.height !== "0px") {
-                content.style.height = "0px";
-                content.style.padding = "0 2%";
-            } else {
-                content.style.height = innerDiv.style.height;
-                content.style.padding = "1% 2% 0 2%";
-            }
-        });
-    }
+	$(".collapsible-content").hide();
+	$(".collapsible").click(function() {
+		$(this).toggleClass("active");
+		$(this).find(".collapsible-content").slideToggle("slow");
+	});
 });
