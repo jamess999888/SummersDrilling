8aea6c1f85efc953a662c1574a05759c1b098206
James McGinnis <mcginnis.james.j@gmail.com>
2020-03-27 20:48:25 -0500

    add the rest of the chevrons and only open one faq at a time

--- js/collapsible.js
+++ js/collapsible.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
 	$(".collapsible-content").hide();
 	$(".collapsible").click(function() {
+		$(".collapsible").removeClass("active");
+		var collapsibleContent = $(this).next(".collapsible-content");
+		$(".collapsible:visible").find("img").attr('src',"/SummersDrilling/images/downArrow.png");
+		$(".collapsible:visible").slideUp();
 		$(this).toggleClass("active");
-		$(this).next(".collapsible-content").slideToggle("slow");
+		if(collapsibleContent.hasClass("active"){
+			collapsibleContent.slideDown();
+			collapsibleContent.find("img").attr('src',"/SummersDrilling/images/upArrow.png");
+		}
 	});
 });
