af60878bb03d759cd0c40e2f66931a3c9059421e
James McGinnis <mcginnis.james.j@gmail.com>
2020-03-27 21:18:31 -0500

    fixing opening one item bug in faq page jquery

--- js/collapsible.js
+++ js/collapsible.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
 	$(".collapsible-content").hide();
+	var openThis;
 	$(".collapsible").click(function() {
+		var openThis = ($(this).next(".collapsible-content:hidden").length);
+		console.log(openThis);
 		$(".collapsible").removeClass("active");
 		var collapsibleContent = $(this).next(".collapsible-content");
-		$(".collapsible:visible").find("img").attr('src',"/SummersDrilling/images/downArrow.png");
-		$(".collapsible:visible").slideUp();
-		$(this).toggleClass("active");
-		if(collapsibleContent.hasClass("active"){
+		$(".collapsible").find("img").attr('src',"/SummersDrilling/images/downArrow.png");
+		$(".collapsible").next(".collapsible-content:visible").slideUp();
+		if(openThis){
 			collapsibleContent.slideDown();
-			collapsibleContent.find("img").attr('src',"/SummersDrilling/images/upArrow.png");
+			$(this).find("img").attr('src',"/SummersDrilling/images/upArrow.png");
+			$(this).addClass("active");
 		}
 	});
 });
